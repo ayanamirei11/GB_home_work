24ca91aff6a52e3029efb0e95546f35a1b98b0e9
Anton_Verchuk <anton_verchuk02@mail.ru>
2022-04-29 15:23:07 +0700

    add code in programe 4

---
+++ program_4/Program.cs
@@ -0,0 +1,27 @@
+﻿string str;
+int a, b, c;
+
+Console.WriteLine("Введите первое число");
+str = Console.ReadLine();
+a = int.Parse(str);
+
+Console.WriteLine("Введите второе число");
+str = Console.ReadLine();
+b = int.Parse(str);
+
+Console.WriteLine("Введите третье число");
+str = Console.ReadLine();
+c = int.Parse(str);
+
+if (a >= b && a >= c)
+{
+    Console.WriteLine("максимальное: " + a);
+}
+else if (b >= a && b >= c)
+{
+    Console.WriteLine("максимальное: " + a);
+}
+else if (c >= a && c >= b)
+{
+    Console.WriteLine("максимальное: " + c);
+}
