00a8b4adf08ae24541ca218e671217fd009a628a
Anton_Verchuk <anton_verchuk02@mail.ru>
2022-04-29 15:37:23 +0700

    add code in programe 6

---
+++ program_6/Program.cs
@@ -0,0 +1,12 @@
+﻿string str;
+int n,i;
+
+Console.WriteLine("Введите число");
+str = Console.ReadLine();
+n=int.Parse(str);
+
+for (i=1; i<=n;i++) {
+    if (i%2==0){
+        Console.WriteLine(i);
+    }   
+}
