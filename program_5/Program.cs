cc6a9b970d36c37aa217ea36d114ea0ab323f480
Anton_Verchuk <anton_verchuk02@mail.ru>
2022-04-29 15:28:49 +0700

    add code in programe 5

---
+++ program_5/Program.cs
@@ -0,0 +1,13 @@
+﻿string str;
+int a;
+
+Console.WriteLine("Введите первое число");
+str = Console.ReadLine();
+a=int.Parse(str);
+
+if (a%2==0){
+    Console.WriteLine("число "+a+" чётное");
+}
+else {
+    Console.WriteLine("число "+a+" нечётное");
+}
