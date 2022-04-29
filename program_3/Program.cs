0758cfb366a5f3f2776bc3efc418d85dec304869
Anton_Verchuk <anton_verchuk02@mail.ru>
2022-04-29 14:49:36 +0700

    add code in programe 3

---
+++ program_3/Program.cs
@@ -0,0 +1,22 @@
+﻿string str;
+int a,b,max,min;
+
+Console.WriteLine("Введите первое число");
+str = Console.ReadLine();
+a=int.Parse(str);
+
+Console.WriteLine("Введите второе число");
+str = Console.ReadLine();
+b=int.Parse(str);
+
+if (a>b){
+    Console.WriteLine("максимальное: "+a);
+    Console.WriteLine("мминимальное: "+b);
+}
+else if (a==b) {
+    Console.WriteLine("числа равные");
+}
+else {
+    Console.WriteLine("максимальное: "+b);
+    Console.WriteLine("мминимальное: "+a);
+}
